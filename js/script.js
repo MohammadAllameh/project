9234f31ec19c75af3826a24716df0abc23b7e03b
MohammadAllameh <rw1387rw@gmail.com>
2022-05-02 23:28:08 +0430

    ADD Project to git

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a+b;
+}
+
+function sayHello() {
+    return ' hello Roocket'
+}
+
+var c =  sum(1 , 3)
+console.log(c);
+
+console.log(sayHello());
+
+var c =  sum(16 , 5)
+console.log(c);
